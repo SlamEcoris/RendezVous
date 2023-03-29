5cdda989a87983da7759806b316ff778835879cf
Michel MARQUETTE <marquettemichel73@gmail.com>
2023-03-29 11:18:47 +0200

    Ajout export BDD

---
+++ bdd/rendezvous.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 29 mars 2023 à 11:13
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `rendezvous`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `nom` tinytext NOT NULL,
+  `prenom` tinytext NOT NULL,
+  `mail` tinytext NOT NULL,
+  `telephone` tinytext NOT NULL,
+  `idCompte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id`, `nom`, `prenom`, `mail`, `telephone`, `idCompte`) VALUES
+(1, 'Client1aa', 'client1bb', 'client1@essai.frcc', '15 47 89 63dd', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+CREATE TABLE `compte` (
+  `id` int(11) NOT NULL,
+  `identifiant` tinytext NOT NULL,
+  `mdp` tinytext NOT NULL,
+  `droit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `compte`
+--
+
+INSERT INTO `compte` (`id`, `identifiant`, `mdp`, `droit`) VALUES
+(1, 'ident', '2118c37356b669d52c22510c0287642551fcdc1b9b27517999e040ad56d1ad678cb71496eb4da19832143ae14ef1ceabf1824349bd608176a91f22f7088a5427', 1),
+(2, 'ident2', '2118c37356b669d52c22510c0287642551fcdc1b9b27517999e040ad56d1ad678cb71496eb4da19832143ae14ef1ceabf1824349bd608176a91f22f7088a5427', 2),
+(3, 'ident3', '2118c37356b669d52c22510c0287642551fcdc1b9b27517999e040ad56d1ad678cb71496eb4da19832143ae14ef1ceabf1824349bd608176a91f22f7088a5427', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employe`
+--
+
+CREATE TABLE `employe` (
+  `id` int(11) NOT NULL,
+  `nom` tinytext NOT NULL,
+  `prenom` tinytext NOT NULL,
+  `mail` tinytext NOT NULL,
+  `telephone` tinytext NOT NULL,
+  `idCompte` int(11) NOT NULL,
+  `idEntreprise` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreprise`
+--
+
+CREATE TABLE `entreprise` (
+  `id` int(11) NOT NULL,
+  `activite` tinytext NOT NULL,
+  `adresse1` tinytext NOT NULL,
+  `adresse2` tinytext NOT NULL,
+  `codePostal` tinytext NOT NULL,
+  `ville` tinytext NOT NULL,
+  `raisonSociale` tinytext NOT NULL,
+  `presentation` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `entreprise`
+--
+
+INSERT INTO `entreprise` (`id`, `activite`, `adresse1`, `adresse2`, `codePostal`, `ville`, `raisonSociale`, `presentation`) VALUES
+(1, 'activite', 'adresse1', 'adresse2', '7400', 'Annecy', 'Raison Sociale', 'Presentation de l entreprise'),
+(2, 'activite', 'adresse1', 'adresse2', '7400', 'Annecy', 'Raison Sociale', 'Presentation de l entreprise');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `horairre`
+--
+
+CREATE TABLE `horairre` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heureDebAM` time NOT NULL,
+  `heureFinAM` time NOT NULL,
+  `heureDebPM` time NOT NULL,
+  `heureFinPM` time NOT NULL,
+  `idEntreprise` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rendezvous`
+--
+
+CREATE TABLE `rendezvous` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heureDebut` time NOT NULL,
+  `heureFin` time NOT NULL,
+  `objet` text NOT NULL,
+  `idClient` int(11) NOT NULL,
+  `idEmploye` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `rendezvous`
+--
+
+INSERT INTO `rendezvous` (`id`, `date`, `heureDebut`, `heureFin`, `objet`, `idClient`, `idEmploye`) VALUES
+(1, '2023-03-19', '08:00:00', '09:00:00', 'Premier rendez-vous', 1, 1),
+(2, '2023-03-30', '11:00:00', '12:00:00', 'second rendez-vous', 1, 2);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `employe`
+--
+ALTER TABLE `employe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `horairre`
+--
+ALTER TABLE `horairre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `compte`
+--
+ALTER TABLE `compte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `employe`
+--
+ALTER TABLE `employe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `horairre`
+--
+ALTER TABLE `horairre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
